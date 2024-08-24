1c3bdf24d049349415eb51a24a6a984fe69ee89a
Zohaib521321 <khanzohaib3231@gmail.com>
2024-08-24 21:58:05 +0500

    Now added customize code

--- lib/app/screens/sale/sale_main.dart
+++ lib/app/screens/sale/sale_main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lawan/app/controller/sales/sales_controller.dart';
+import 'package:lawan/app/screens/sale/widgets/monthly_sales_tabbar.dart';
 import 'package:lawan/app/screens/sale/widgets/sale_header.dart';
+import 'package:lawan/app/screens/sale/widgets/sales_tabbar.dart';
 import 'package:lawan/app/utils/images.dart';
 import 'package:lawan/app/utils/responsive_utils.dart';
+import 'package:lawan/app/widgets/Areena/custom_tabbar.dart';
 import 'package:lawan/app/widgets/chart/bar_chart.dart';
 import 'package:lawan/app/widgets/container/circular_container.dart';
 import 'package:lawan/app/widgets/container/sales_container.dart';
@@ -20,7 +23,12 @@ class SaleMain extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment(0.00, -1.00),
           end: Alignment(0, 1),
-          colors: [Color(0xCC0C1B22), Colors.white],
+          colors: [
+            Color(0xCC0C1B22), // Start color
+            Color(0x0044D8BE), // Gradient color at 30%
+            Color(0xfff2f3f2), // White color for the remaining 70%
+          ],
+          stops: [0.0, 0.12, 0.13], // Defines the stops for each color
         ),
       ),
       child: Padding(
@@ -37,322 +45,33 @@ class SaleMain extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                CircularContainer(
-                  svgPath: Images.left,
-                  onTap: controller.selectPreviousMonth,
-                ),
-                SizedBox(
-                  width: Get.height * 0.01,
-                ),
-                CircularContainer(
-                  svgPath: Images.right,
-                  onTap: controller.selectNextMonth,
-                ),
-                SizedBox(
-                  width: Get.height * 0.01,
-                ),
-                Obx(() => Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 1),
-                        decoration: ShapeDecoration(
-                            color: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(80),
-                            ),
-                            shadows: [
-                              const BoxShadow(
-                                color: Color(0x0C000000),
-                                blurRadius: 2,
-                                offset: Offset(0, 1),
-                              )
-                            ]),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton(
-                            icon: Image.asset(
-                              Images
-                                  .chevron_sort, // Ensure this path is correct
-                              width: 20,
-                              height: 20,
-                              color: Colors.white,
-                            ),
-                            dropdownColor: Colors.black,
-                            value: controller.selectedMonth,
-                            onChanged: (value) {
-                              if (value != null) {
-                                controller.updateSelectedMonth(value);
-                              }
-                            },
-                            items: controller.monthsName.map((month) {
-                              return DropdownMenuItem(
-                                child: Row(
-                                  children: [
-                                    const Icon(
-                                      Icons.calendar_today_outlined,
-                                      color: Color(0xffACB4C0),
-                                      size: 16,
-                                    ),
-                                    const SizedBox(width: 8),
-                                    Text(
-                                      month,
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 15,
-                                        fontFamily: 'Lufga',
-                                        fontWeight: FontWeight.w500,
-                                        height: 0.05,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                value: month,
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ),
-                    )),
-                SizedBox(
-                  width: Get.height * 0.01,
-                ),
-                CircularContainer(
-                  bgColor: Colors.black,
-                  borderColor: Colors.black,
-                  svgPath: Images.upload,
-                  onTap: () {},
+                MonthlySalesTabBar(controller: controller),
+                Expanded(
+                  flex: 2,
+                  child: CircularContainer(
+                    bgColor: Colors.black,
+                    borderColor: Colors.black,
+                    svgPath: Images.upload,
+                    onTap: () {},
+                  ),
                 ),
               ],
             ),
             SizedBox(
               height: Get.height * 0.02,
             ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
-              child: Padding(
-                padding: const EdgeInsets.all(14.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                            child: Container(
-                          decoration: ShapeDecoration(
-                            shape: RoundedRectangleBorder(
-                              side: const BorderSide(
-                                  width: 1, color: Color(0xFFDEE1E2)),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const TextWidget(
-                                  title: "Today",
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                  textAlign: TextAlign.start,
-                                  textColor: Color(0xFF545F71),
-                                ),
-                                const TextWidget(
-                                  title: "RM120",
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  textAlign: TextAlign.start,
-                                ),
-                                SizedBox(
-                                  height: Get.height * 0.01,
-                                ),
-                                Row(
-                                  children: [
-                                    Container(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 4, horizontal: 8),
-                                      decoration: ShapeDecoration(
-                                        shape: RoundedRectangleBorder(
-                                          side: const BorderSide(
-                                              width: 1,
-                                              color: Color(0xFFDEE1E2)),
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                        ),
-                                      ),
-                                      child: const TextWidget(
-                                        title: "- 5%",
-                                        textColor: Color(0xFFF05B5B),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: Get.width * 0.02,
-                                    ),
-                                    const TextWidget(
-                                      title: "vs yesterday",
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                      textAlign: TextAlign.start,
-                                      textColor: Color(0xFF545F71),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        )),
-                        SizedBox(
-                          width: Get.width * 0.03,
-                        ),
-                        Expanded(
-                            child: Container(
-                          decoration: ShapeDecoration(
-                            shape: RoundedRectangleBorder(
-                              side: const BorderSide(
-                                  width: 1, color: Color(0xFFDEE1E2)),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const TextWidget(
-                                  title: "Weekly Sales",
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                  textAlign: TextAlign.start,
-                                  textColor: Color(0xFF545F71),
-                                ),
-                                const TextWidget(
-                                  title: "RM32,140",
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  textAlign: TextAlign.start,
-                                ),
-                                SizedBox(
-                                  height: Get.height * 0.01,
-                                ),
-                                Row(
-                                  children: [
-                                    Container(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 4, horizontal: 8),
-                                      decoration: ShapeDecoration(
-                                        shape: RoundedRectangleBorder(
-                                          side: const BorderSide(
-                                              width: 1,
-                                              color: Color(0xFFDEE1E2)),
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                        ),
-                                      ),
-                                      child: const TextWidget(
-                                        title: "+ 20%",
-                                        textColor: Color(0xFF23A891),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: Get.width * 0.02,
-                                    ),
-                                    const TextWidget(
-                                      title: "vs last week",
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                      textAlign: TextAlign.start,
-                                      textColor: Color(0xFF545F71),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        )),
-                      ],
-                    ),
-                    SizedBox(
-                      height: Get.height * 0.01,
-                    ),
-                    CustomChart(),
-                  ],
-                ),
-              ),
-            ),
+            const CustomChart(),
             SizedBox(
               height: Responsive.h2,
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFFDEE1E2),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              padding: const EdgeInsets.all(2),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: List.generate(3, (index) {
-                  return Obx(() => Container(
-                        decoration: BoxDecoration(
-                          color: controller.tabBarIndex.value == index
-                              ? Colors.white
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 22, vertical: 12),
-                        child: InkWell(
-                          splashColor: Colors.transparent,
-                          onTap: () {
-                            controller.selectTabBar(index);
-                          },
-                          child: Row(
-                            children: [
-                              TextWidget(
-                                title: controller.tabBarItem[index],
-                                textColor: controller.tabBarIndex.value == index
-                                    ? Colors.black
-                                    : const Color(0xFF545F71),
-                              ),
-                              controller.tabBarItem[index] == "Ongoing"
-                                  ? SizedBox(
-                                      width: Responsive.h1,
-                                    )
-                                  : Container(),
-                              controller.tabBarItem[index] == "Ongoing"
-                                  ? Container(
-                                      padding: const EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(18),
-                                          color: Colors.transparent,
-                                          border: Border.all(
-                                              color: controller
-                                                          .tabBarIndex.value ==
-                                                      index
-                                                  ? Colors.black
-                                                  : Colors.white)),
-                                      child: TextWidget(
-                                        title: "257",
-                                        textColor:
-                                            controller.tabBarIndex.value ==
-                                                    index
-                                                ? Colors.black
-                                                : const Color(0xFF545F71),
-                                      ),
-                                    )
-                                  : const SizedBox.shrink()
-                            ],
-                          ),
-                        ),
-                      ));
-                }),
-              ),
-            ),
+            CustomSalesTabBar(
+                controller: controller, tabBarItems: controller.tabBarItem),
             SizedBox(
               height: Responsive.h1,
             ),
             Row(
               children: [
-                TextWidget(
+                const TextWidget(
                   title: "24 Sep",
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
@@ -360,7 +79,7 @@ class SaleMain extends StatelessWidget {
                 SizedBox(
                   width: Responsive.h2,
                 ),
-                TextWidget(
+                const TextWidget(
                   title: "Mon, Today",
                   textColor: Color(0xFF545F71),
                 )
@@ -369,7 +88,9 @@ class SaleMain extends StatelessWidget {
             SizedBox(
               height: Responsive.h1,
             ),
-            SalesContainer()
+            SalesContainer(
+              salesController: controller,
+            )
           ],
         ),
       ),
