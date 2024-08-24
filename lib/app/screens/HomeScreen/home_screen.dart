7c5f97f42ce39228ae762f2553759807f826ab12
Zohaib521321 <khanzohaib3231@gmail.com>
2024-08-24 21:56:31 +0500

    Added safe area for better looking

--- lib/app/screens/HomeScreen/home_screen.dart
+++ lib/app/screens/HomeScreen/home_screen.dart
@@ -35,67 +35,67 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<BottomNavigationController>();
-    return Scaffold(
-        backgroundColor: const Color(0xFFF2F3F2),
-        body: ColorFiltered(
-          colorFilter: ColorFilter.mode(
-            Colors.transparent.withOpacity(0.001), // Simulate brightness
-            BlendMode.colorBurn,
-          ),
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.0, 0.8), // Start near the bottom
-                end: Alignment(0.0, 1), // End at the bottom edge
-                colors: [
-                  Colors.black.withOpacity(0.0), // Fully transparent color
-                  Colors.black.withOpacity(0.4), // Slightly visible gradient
-                ],
-              ),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: const Color(0xFFF2F3F2),
+          body: ColorFiltered(
+            colorFilter: ColorFilter.mode(
+              Colors.transparent.withOpacity(0.001), // Simulate brightness
+              BlendMode.colorBurn,
             ),
-            child: Stack(children: [
-              SingleChildScrollView(
-                child: SafeArea(
-
-                  child: Obx(() {
-                    if (controller.selectedItem.value == "Arena") {
-                      return Padding(
-                        padding: EdgeInsets.only(
-                            top: MediaQuery.of(context).size.width * 0.02,
-                            left: MediaQuery.of(context).size.width * 0.03,
-                            right: MediaQuery.of(context).size.width * 0.03),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            ScreenHeader(),
-                            SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.01),
-                            ScreenBody(),
-                          ],
-                        ),
-                      );
-                    } else if (controller.selectedItem.value == "Sales") {
-                      return SaleMain();
-                    } else {
-                      return Sessionscreen();
-                    }
-                  }),
-
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.0, 0.8), // Start near the bottom
+                  end: Alignment(0.0, 1), // End at the bottom edge
+                  colors: [
+                    Colors.black.withOpacity(0.0), // Fully transparent color
+                    Colors.black.withOpacity(0.4), // Slightly visible gradient
+                  ],
                 ),
               ),
-              CustomBottomNavigation(
-                textStyle: TextStyle(
-                  fontSize: 15,
-                  fontFamily: 'Lufga',
-                  fontWeight: FontWeight.w400,
-                  height: 1,
-                  letterSpacing: -0.24,
+              child: Stack(children: [
+                SingleChildScrollView(
+                  child: SafeArea(
+                    child: Obx(() {
+                      if (controller.selectedItem.value == "Arena") {
+                        return Padding(
+                          padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.width * 0.02,
+                              left: MediaQuery.of(context).size.width * 0.03,
+                              right: MediaQuery.of(context).size.width * 0.03),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ScreenHeader(),
+                              SizedBox(
+                                  height: MediaQuery.of(context).size.height *
+                                      0.01),
+                              ScreenBody(),
+                            ],
+                          ),
+                        );
+                      } else if (controller.selectedItem.value == "Sales") {
+                        return SaleMain();
+                      } else {
+                        return Sessionscreen();
+                      }
+                    }),
+                  ),
                 ),
-              ),
-            ]),
-          ),
-        ));
+                CustomBottomNavigation(
+                  textStyle: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Lufga',
+                    fontWeight: FontWeight.w400,
+                    height: 1,
+                    letterSpacing: -0.24,
+                  ),
+                ),
+              ]),
+            ),
+          )),
+    );
   }
 }
