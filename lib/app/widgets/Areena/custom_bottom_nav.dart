2666fc06bb0c0b37f17b2ce823653b0dfef684c5
Muhammad Zohaib <Khanzohaib3231@gmail.com>
2024-08-27 10:14:22 +0500

    Custom bottom nav now using getx in main screens

--- lib/app/widgets/Areena/custom_bottom_nav.dart
+++ lib/app/widgets/Areena/custom_bottom_nav.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
-import 'package:lawan/app/controller/bottom_bar/bottom_navigation_controller.dart';
 
 import '../../screens/HomeScreen/Widgets/circularContainer.dart';
-import '../../utils/images.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   final double widthFactor;
@@ -16,10 +13,14 @@ class CustomBottomNavigation extends StatelessWidget {
   final double iconSize;
   final double spacing;
   final TextStyle textStyle;
+  final String selectedItem;
+  final List<String> imagePath;
+  final List<String> title;
+  final Function(String) onItemSelected;
 
   const CustomBottomNavigation({
     Key? key,
-    this.widthFactor = 0.5, // Adjust this value based on screen width
+    this.widthFactor = 0.56, // Adjust this value based on screen width
     this.containerHeight = 70.0,
     this.padding = 4.0,
     this.containerColor = const Color(0xCCE5E6E5),
@@ -28,14 +29,17 @@ class CustomBottomNavigation extends StatelessWidget {
     this.iconSize = 15.0,
     this.spacing = 6.0,
     required this.textStyle,
-  }) : super(key: key);
+    required this.selectedItem,
+    required this.imagePath,
+    required this.title,
+    required this.onItemSelected,
+  })  : assert(imagePath.length == title.length,
+            "Image paths and titles must have the same length."),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.find<BottomNavigationController>();
     return Positioned(
-      left:
-          MediaQuery.of(context).size.width / 3.9, // Adjust position as needed
       bottom: 10,
       child: Container(
         margin: margin,
@@ -49,75 +53,29 @@ class CustomBottomNavigation extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Obx(() => Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: CircularContainer(
-                      heightpadding:3,
-                      image: Images.play,
-                      color: controller.selectedItem.value == "Session"
-                          ? Colors.black
-                          : Colors.white,
-                      imageHeight: iconSize,
-                      textColor: controller.selectedItem.value == "Session"
-                          ? Colors.white
-                          : Colors.black,
-                      text: "Session",
-                      imageWidth: 15,
-
-                      onTap: () {
-                        controller.changeSelection("Session");
-                      },
-                      // textStyle: textStyle,
-                    ),
-                  ),
-                  SizedBox(
-                    width: spacing,
-                  ),
-                  Expanded(
-                    child: CircularContainer(
-                      heightpadding: 2,
-                      image: Images.areena, // Replace with your asset path
-                      color: controller.selectedItem.value == "Arena"
-                          ? Colors.black
-                          : Colors.white,
-                      text: "Arena",
-                      textColor: controller.selectedItem.value == "Arena"
-                          ? Colors.white
-                          : Colors.black,
-                      imageHeight: iconSize,
-                      imageWidth: iconSize,
-                      onTap: () {
-                        controller.changeSelection("Arena");
-                      },
-                      // textStyle: textStyle,
-                    ),
-                  ),
-                  SizedBox(
-                    width: spacing,
-                  ),
-                  Expanded(
-                    child: CircularContainer(
-                      heightpadding:2,
-                      image:"${Images.mainImage}/sales.svg", // Replace with your asset path
-                      color: controller.selectedItem.value == "Sales"
-                          ? Colors.black
-                          : Colors.white,
-                      text: "Sales",
-                      textColor: controller.selectedItem.value == "Sales"
-                          ? Colors.white
-                          : Colors.black,
-                      imageHeight: iconSize,
-                      imageWidth: iconSize,
-                      onTap: () {
-                        controller.changeSelection("Sales");
-                      },
-                      // textStyle: textStyle,
-                    ),
-                  ),
-                ],
-              )),
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: title.length,
+            itemBuilder: (context, index) {
+              return CircularContainer(
+                heightpadding: 2,
+                image:
+                    imagePath[index], // Use imagePath from the constructor list
+                color:
+                    selectedItem == title[index] ? Colors.black : Colors.white,
+                text: title[index], // Use title from the constructor list
+                textColor:
+                    selectedItem == title[index] ? Colors.white : Colors.black,
+                imageHeight: iconSize,
+                imageWidth: iconSize,
+                onTap: () {
+                  onItemSelected(
+                      title[index]); // Call the provided callback function
+                },
+              );
+            },
+            separatorBuilder: (context, index) => SizedBox(width: spacing),
+          ),
         ),
       ),
     );
