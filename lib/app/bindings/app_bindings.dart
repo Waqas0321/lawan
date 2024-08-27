db08f052fe08e796a9b8808cf79b8ac860d3592b
Muhammad Zohaib <Khanzohaib3231@gmail.com>
2024-08-27 10:12:52 +0500

    app binding updated

--- lib/app/bindings/app_bindings.dart
+++ lib/app/bindings/app_bindings.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:lawan/app/controller/bottom_bar/bottom_navigation_controller.dart';
+import 'package:lawan/app/controller/friendly/freindly_bottom_nav_controller.dart';
 import '../controller/Session_Screen/SessionScreen_Controller.dart';
 import '../controller/sales/sales_controller.dart';
 import 'package:lawan/app/controller/manual/MenualBooking_Controller.dart';
@@ -8,11 +9,11 @@ class InitialBindings extends Bindings {
   @override
   void dependencies() {
     Get.put(BottomNavigationController(), permanent: true);
+    Get.put(FriendlyBottomNavController(), permanent: true);
 
-    Get.put(SalesController());
+    Get.put(SalesController(), permanent: true);
     // Menual Booking
-    Get.put(MenualBookinController());
-    Get.put(SessionScreenController());
-
+    Get.put(MenualBookinController(), permanent: true);
+    Get.put(SessionScreenController(), permanent: true);
   }
 }
