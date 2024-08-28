63ba78f4cf76b2f4565820969a8e96fb6234dfe8
Muhammad Zohaib <Khanzohaib3231@gmail.com>
2024-08-29 01:45:28 +0500

    share invite screen started

---
+++ lib/app/screens/share_invite/share_invite_private_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShareInvitePrivateScreen extends StatelessWidget {
+  const ShareInvitePrivateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
